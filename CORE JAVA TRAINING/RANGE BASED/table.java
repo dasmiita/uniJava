c3ed6359276f920feb7d5aab6b90231d4fcffd50
dasmiita <185056260+dasmiita@users.noreply.github.com>
2025-02-24 10:28:05 +0530

    Add files via upload

---
+++ CORE JAVA TRAINING/RANGE BASED/table.java
@@ -0,0 +1,15 @@
+//program to print multipl
+public class table {
+    public static void main(String[] args) {
+        int mul=0;
+    
+    int n=10;
+    for(int i=1;i<=n;i++){
+        for (i=1;i<=10;i++){
+           mul =  (n*i);
+           System.out.println(mul);
+
+        }
+        System.out.println(mul);
+    }
+}}
