4c13dd943c9cc05a074b50a0d60631ef67a0f9f8
dasmiita <185056260+dasmiita@users.noreply.github.com>
2025-02-24 10:24:49 +0530

    Add files via upload

---
+++ CORE JAVA TRAINING/RANGE BASED/even.java
@@ -0,0 +1,13 @@
+//program to display even numbers from 1 to 20
+public class even {
+    public static void main(String[] args){
+        System.out.println("list of even numbers are = ");
+        for(int i=1;i<=20;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+
+    }
+    
+}
---
+++ CORE JAVA TRAINING/RANGE BASED/fact.java
@@ -0,0 +1,15 @@
+//program to print factorial for number 2 to 7
+public class fact {
+    public static void main(String[] args) {
+        int fact=1;
+        for(int i=2;i<=7;i++)   
+        {
+            for(i=2;i<=7;i++){
+                fact = fact * i;
+                System.out.println("factorial of " + i + " is " + fact);
+            }
+    
+        }
+    }
+    
+}
